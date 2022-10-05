98972657e79a2da0eefaf7ce5f956b9f242b397d
21 Tech Enterprise <21techenterprise@Macintosh.local>
2022-10-05 09:15:15 +0700

    first commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import { Facebook1 } from './Facebook1';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -17,6 +18,7 @@ const Layout = ({ children }) => {
         {children}
       </main>
       <footer>
+      <Facebook1/>
         <Footer />
       </footer>
     </div>
